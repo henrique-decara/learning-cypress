c5c43ccc67454945aecd3bdc02bd876479654bb3
Henrique de Cara <henrique.decara@windriver.com>
2024-09-17 22:01:17 -0300

    adds env in the config file, adds login fixture

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    // implement node event listeners here
+    myBaseUrl: "..\\..\\index.html",
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,23 @@
 const logins = require('../fixtures/loginFixture.json')
 
-describe('Testing the login form', () => {  
-  
-  it('should insert valid credentials', () => {
-    cy.visit('..\\..\\index.html')
-    let email_field = cy.get('#email')
-    let email = cy.fixture('loginFixture')
-    console.log("hey, here is my email", email)
-    //email_field.type("admin@user.com")
+describe('Testing the login form', () => { 
+
+  before(() => {
+    cy.visit(Cypress.env('myBaseUrl'))
+    //TODO: Implement fixtures so that I can reuse it on the login page
+})
+  it('Should login successfully', () => {
+    cy.fixture('loginFixture.json').then((user) => {
+      cy.get('#email').type(user[valid_email]);
+      cy.get('#password').type(user);
+    })
+
+    //   cy.get('#email').type(user.valid_email),
+    //   cy.get('#password').type(user.password)
+    // })
+
+    cy.get('[type="button"]').click()
+    console.log("eu vivi h'a 10 mil anos atras")
   })
+
 })
