e0ccea14984289f925908491f1ff716f9290abe1
Henrique de Cara <henrique.decara@windriver.com>
2024-09-23 19:34:16 -0300

    set the user credentials using the json fixture file

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,20 +4,13 @@ describe('Testing the login form', () => {
 
   before(() => {
     cy.visit(Cypress.env('myBaseUrl'))
-    //TODO: Implement fixtures so that I can reuse it on the login page
 })
   it('Should login successfully', () => {
     cy.fixture('loginFixture.json').then((user) => {
-      cy.get('#email').type(user[valid_email]);
-      cy.get('#password').type(user);
+      cy.get('#email').type(user.valid_email);
+      cy.get('#password').type(user.password);
     })
-
-    //   cy.get('#email').type(user.valid_email),
-    //   cy.get('#password').type(user.password)
-    // })
-
     cy.get('[type="button"]').click()
-    console.log("eu vivi h'a 10 mil anos atras")
   })
 
 })
